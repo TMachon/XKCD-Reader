87dfc76de83fcfe1aae4c10a71ccd9acc9076083
TMachon <theo.machon.91@gmail.com>
2019-02-08 15:25:59 +0100

    Showing a RecylclerView made of tests

--- app/src/main/java/com/example/xkcdreader/view/MainActivity.java
+++ app/src/main/java/com/example/xkcdreader/view/MainActivity.java
@@ -3,9 +3,17 @@ package com.example.xkcdreader.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.*;
+
 import com.example.xkcdreader.R;
 import com.example.xkcdreader.controller.MainController;
 import com.example.xkcdreader.model.*;
+import com.example.xkcdreader.view.adapter.AdapterMainLayout;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -13,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
     // Attributes
 
     private MainController controller;
-    public TextView text;
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     // Methods
 
@@ -26,6 +36,22 @@ public class MainActivity extends AppCompatActivity {
         controller = new MainController(this);
         //controller.promptLastComic(this);
         //controller.promptComicWithId(this, 18);
+
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        // use this setting to
+        // improve performance if you know that changes
+        // in content do not change the layout size
+        // of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }// define an adapter
+        mAdapter = new AdapterMainLayout(input);
+        recyclerView.setAdapter(mAdapter);
 
     }
 }
